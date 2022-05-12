9c94c9597c70c4ddfdf5ffde0b50728556573bf0
Mudassir Nazir <mudassir.nazir@visionx.io>
2022-05-12 17:18:49 +0500

    changes

--- routes/categories.js
+++ routes/categories.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const Categories = require('../controllers/Categories');
 
 
+router.get('/getById/:id', Categories.getById);
 router.get('/categoriesList', Categories.categoriesList);
 router.post('/newCategorie', Categories.newCategorie);
-router.get('/getById/:id', Categories.getById);
+
 
 
 module.exports= router;
